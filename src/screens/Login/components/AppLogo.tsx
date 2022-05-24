5786d14b4cb3df5d0a1845f6134d64b473af7a66
George Karcivadze <karcivadzega@gmail.com>
2022-05-24 17:39:03 +0300

    added applogo change onPress

--- src/screens/Login/components/AppLogo.tsx
+++ src/screens/Login/components/AppLogo.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import {
   View,
   Image,
@@ -9,16 +10,25 @@ import {
 import styles from './AppLogo.styles';
 
 const AppLogo: React.FC = () => {
-  const logoImageUri = 'https://i.pinimg.com/750x/18/e3/4e/18e34e354c421ffb316f25d96d7673f8.jpg';
+  const imageUri = {
+    guts: 'https://i.pinimg.com/750x/18/e3/4e/18e34e354c421ffb316f25d96d7673f8.jpg',
+    griffith: 'https://w0.peakpx.com/wallpaper/963/790/HD-wallpaper-femto-berserk-casca-griffith-guts-manga.jpg',
+  };
+
+  const [logoImageUri, setLogoImageUri] = useState<string>(imageUri.guts);
 
   return (
     <View style={styles.logoContainer}>
       <TouchableWithoutFeedback
         onLongPress={() => Linking.openURL('https://myanimelist.net/')}>
-        <Image
-          style={styles.image}
-          source={{ uri: logoImageUri }}
-        />
+        <TouchableWithoutFeedback
+          onPress={() => setLogoImageUri((prev) => prev === imageUri.guts ? imageUri.griffith : imageUri.guts)}
+        >
+          <Image
+            style={styles.image}
+            source={{ uri: logoImageUri }}
+          />
+        </TouchableWithoutFeedback>
       </TouchableWithoutFeedback>
     </View>
   );
