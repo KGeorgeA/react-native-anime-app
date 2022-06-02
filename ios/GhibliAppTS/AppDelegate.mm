8ba98754d2ebe4a264d1b1ff3ade9b49a2f4f92f
George Karcivadze <karcivadzega@gmail.com>
2022-06-02 14:27:32 +0300

    added maps dependenices and a request for usage

--- ios/GhibliAppTS/AppDelegate.mm
+++ ios/GhibliAppTS/AppDelegate.mm
@@ -6,6 +6,8 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -29,6 +31,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyA_-0pnYq3cdEMWeYPLUdLrK71Yq7DLdFo"];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
